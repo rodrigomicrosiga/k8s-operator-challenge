db634d1b76a5f1853e91bffb4f7c06cd75386c36
rodrigomicrosiga <rodrigo.microsiga@icloud.com>
2026-05-13 17:47:50 -0300

    Correções diversas envolvendo go build, packages, go mod, controller_test e suite_test

--- README.md
+++ README.md
@@ -31,3 +31,33 @@ internal/suite_test.go
 RESUMO DA ETAPA 1: 
 Repositório Go com api/, controllers/, config/ e utilização de comandos make para gerar manifests.
 
+Ao realizar as alterações nos códigos abaixo: 
+
+- operatorchallenge_types.go
+- operatorchallenge_controller.go
+
+Foi necessário realizar os processos make generate e make manifests, porém obtive o problema abaixo:
+
+"/home/rodrigobsantos@sp01.local/code/k8s-operator-challenge/bin/controller-gen" object:headerFile="hack/boilerplate.go.txt",year=2026 paths="./..."
+cmd/main.go:39:2: found packages controllers (operatorchallenge_controller.go) and controller (suite_test.go) in /home/rodrigobsantos@sp01.local/code/k8s-operator-challenge/internal/controller
+Error: not all generators ran successfully
+run `controller-gen object:headerFile=hack/boilerplate.go.txt,year=2026 paths=./... -w` to see all available markers, or `controller-gen object:headerFile=hack/boilerplate.go.txt,year=2026 paths=./... -h` for usage
+make: *** [Makefile:52: generate] Erro 1
+
+Procurando entender mais o erro, foi possivel analisar que o controller_test e suite_test estavam importando packges diferentes e isso provocava o erro.
+
+No momento de geração do go build ./... várias inconsistências foram apresentadas (essa etapa exige mais estudo e praticar)
+
+Resumo simples de correções:
+
+- logger: agora é criado logo no início do Reconcile com logger := log.FromContext(ctx) e usado em todo o método.
+- challengev1: o código usa o alias correto para o pacote api/v1 (challengev1), removendo a referência incorreta appsv1alpha1.
+- svcKey: variável removida porque não estava sendo usada, em vez disso, quando necessário, eu construo types.NamespacedName inline nas chamadas de log.
+- Tratamento de erros e logs: adicionei logs mais descritivos para facilitar debug. (necessário revisão)
+
+Após os pontos acima foi executado:
+gofmt -w internal/controller/operatorchallenge_controller.go
+go build ./...
+make generate
+make manifests
+go run ./main.go
--- internal/controller/operatorchallenge_controller.go
+++ internal/controller/operatorchallenge_controller.go
@@ -1,30 +1,20 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package controller
+package controllers
 
 import (
 	"context"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	appsv1 "github.com/rodrigomicrosiga/k8s-operator-challenge/api/v1"
+	challengev1 "github.com/rodrigomicrosiga/k8s-operator-challenge/api/v1"
 )
 
 // OperatorChallengeReconciler reconciles a OperatorChallenge object
@@ -33,31 +23,141 @@ type OperatorChallengeReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=apps.teste.com,resources=operatorchallenges,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=apps.teste.com,resources=operatorchallenges/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=apps.teste.com,resources=operatorchallenges/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps.rodrigo.com,resources=operatorchallenges,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps.rodrigo.com,resources=operatorchallenges/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=apps.rodrigo.com,resources=operatorchallenges/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the OperatorChallenge object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.23.3/pkg/reconcile
 func (r *OperatorChallengeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = logf.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// 1. Buscar o CR OperatorChallenge
+	var cr challengev1.OperatorChallenge
+	if err := r.Get(ctx, req.NamespacedName, &cr); err != nil {
+		// recurso pode ter sido deletado
+		logger.Error(err, "unable to fetch OperatorChallenge", "namespacedName", req.NamespacedName)
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// 2. Construir o Deployment desejado e garantir owner reference
+	desiredDep := r.deploymentFor(&cr)
+	if err := controllerutil.SetControllerReference(&cr, desiredDep, r.Scheme); err != nil {
+		logger.Error(err, "unable to set owner reference on Deployment")
+		return ctrl.Result{}, err
+	}
+
+	var existingDep appsv1.Deployment
+	depKey := types.NamespacedName{Name: desiredDep.Name, Namespace: desiredDep.Namespace}
+	op, err := controllerutil.CreateOrUpdate(ctx, r.Client, &existingDep, func() error {
+		// manter labels e spec do desiredDep
+		existingDep.Labels = desiredDep.Labels
+		existingDep.Spec = desiredDep.Spec
+		return nil
+	})
+	if err != nil {
+		logger.Error(err, "failed to create or update Deployment", "deployment", depKey)
+		return ctrl.Result{}, err
+	}
+	logger.Info("Deployment reconciled", "operation", op, "deployment", depKey)
+
+	// 3. Construir o Service desejado e garantir owner reference
+	desiredSvc := r.serviceFor(&cr)
+	if err := controllerutil.SetControllerReference(&cr, desiredSvc, r.Scheme); err != nil {
+		logger.Error(err, "unable to set owner reference on Service")
+		return ctrl.Result{}, err
+	}
+
+	var existingSvc corev1.Service
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, &existingSvc, func() error {
+		existingSvc.Labels = desiredSvc.Labels
+		existingSvc.Spec = desiredSvc.Spec
+		return nil
+	})
+	if err != nil {
+		logger.Error(err, "failed to create or update Service", "service", types.NamespacedName{Name: desiredSvc.Name, Namespace: desiredSvc.Namespace})
+		return ctrl.Result{}, err
+	}
+	logger.Info("Service reconciled", "service", types.NamespacedName{Name: desiredSvc.Name, Namespace: desiredSvc.Namespace})
+
+	// 4. Atualizar status com readyReplicas do Deployment
+	var updatedDep appsv1.Deployment
+	if err := r.Get(ctx, depKey, &updatedDep); err == nil {
+		ready := updatedDep.Status.ReadyReplicas
+		if cr.Status.ReadyReplicas != ready {
+			cr.Status.ReadyReplicas = ready
+			if err := r.Status().Update(ctx, &cr); err != nil {
+				logger.Error(err, "failed to update OperatorChallenge status")
+				return ctrl.Result{}, err
+			}
+			logger.Info("OperatorChallenge status updated", "readyReplicas", ready)
+		}
+	} else {
+		// se não conseguiu obter o deployment, loga e continua (CreateOrUpdate já tratou criação)
+		logger.Error(err, "failed to get Deployment for status update", "deployment", depKey)
+	}
 
 	return ctrl.Result{}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
+func (r *OperatorChallengeReconciler) deploymentFor(cr *challengev1.OperatorChallenge) *appsv1.Deployment {
+	labels := map[string]string{"app": cr.Name}
+
+	replicas := int32(1)
+	if cr.Spec.Replicas != nil {
+		replicas = *cr.Spec.Replicas
+	}
+
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-deployment",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{MatchLabels: labels},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: labels},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "app",
+							Image: cr.Spec.Image,
+							Ports: []corev1.ContainerPort{
+								{ContainerPort: cr.Spec.Port},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (r *OperatorChallengeReconciler) serviceFor(cr *challengev1.OperatorChallenge) *corev1.Service {
+	labels := map[string]string{"app": cr.Name}
+
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-svc",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: labels,
+			Ports: []corev1.ServicePort{
+				{Port: cr.Spec.Port},
+			},
+			Type: corev1.ServiceTypeClusterIP,
+		},
+	}
+}
+
 func (r *OperatorChallengeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appsv1.OperatorChallenge{}).
-		Named("operatorchallenge").
+		For(&challengev1.OperatorChallenge{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Service{}).
 		Complete(r)
 }
